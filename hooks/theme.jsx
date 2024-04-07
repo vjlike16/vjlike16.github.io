28e741ab6074e4828e078e7dd5a41ba819397b93
Vijay Kumar Sharma <vjlike16@gmail.com>
2024-04-07 16:02:53 +0530

    updated theme hook

--- hooks/theme.jsx
+++ hooks/theme.jsx
@@ -2,21 +2,31 @@ import { useEffect, useState } from 'react';
 
 const useTheme = () => {
   const [theme, setTheme] = useState(() => {
-    const storedTheme = localStorage.getItem('theme');
-    return storedTheme || 'light-theme';
+    if (typeof window !== 'undefined') {
+      const storedTheme = localStorage.getItem('theme');
+      return storedTheme || 'light-theme';
+    } else {
+      return 'light-theme';
+    }
   });
   const toggleTheme = () => {
     const newTheme = theme === 'light-theme' ? 'dark-theme' : 'light-theme';
-    localStorage.setItem('theme', newTheme);
+    if (typeof window !== 'undefined') {
+      localStorage.setItem('theme', newTheme);
+    }
     setTheme(newTheme);
   };
 
   useEffect(() => {
-    const root = window.document.documentElement;
-    root.classList.remove(
-      theme === 'light-theme' ? 'dark-theme' : 'light-theme'
-    );
-    root.classList.add(theme === 'light-theme' ? 'light-theme' : 'dark-theme');
+    if (typeof window !== 'undefined') {
+      const root = window.document.documentElement;
+      root.classList.remove(
+        theme === 'light-theme' ? 'dark-theme' : 'light-theme'
+      );
+      root.classList.add(
+        theme === 'light-theme' ? 'light-theme' : 'dark-theme'
+      );
+    }
   }, [theme]);
 
   return [theme, toggleTheme];
